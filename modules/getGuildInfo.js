217f417fb6c95ddc2e30053fc4caeb4aa4fe58cb
minsikjeong <wjdalstlr21@gmail.com>
2024-04-25 19:45:43 +0900

    채널별로 작동할수 있도록 수정

--- modules/getGuildInfo.js
+++ modules/getGuildInfo.js
@@ -1,7 +1,26 @@
 const guildInfo = [
-  {guildId: process.env.GUILD_ID, adminRole: process.env.GUILD_ADMIN_ROLE_ID, guestRole: process.env.GUILD_GUEST_ROLE_ID, guildMemberRole: process.env.GUILD_ROLE, roleAuditingChannelId: process.env.ROLE_AUDITING_CHANNEL_ID, channelId: process.env.CHANNEL_ID, partyChannelId: process.env.PARTY_RECRUITMENT}
-  , {guildId: process.env.DEV_GUILD_ID, adminRole: process.env.DEV_GUILD_ADMIN_ROLE_ID, guestRole: process.env.DEV_GUILD_GUEST_ROLE_ID, guildMemberRole: process.env.DEV_GUILD_ROLE, roleAuditingChannelId: process.env.DEV_ROLE_AUDITING_CHANNEL_ID, channelId: process.env.DEV_CHANNEL_ID, partyChannelId: process.env.DEV_PARTY_RECRUITMENT}
+  // 향내나는실크
+  {
+    guildId: process.env.GUILD_ID,
+    adminRole: process.env.GUILD_ADMIN_ROLE_ID,
+    guestRole: process.env.GUILD_GUEST_ROLE_ID,
+    guildMemberRole: process.env.GUILD_ROLE,
+    roleAuditingChannelId: process.env.ROLE_AUDITING_CHANNEL_ID,
+    channelId: process.env.CHANNEL_ID,
+    partyChannelId: process.env.PARTY_RECRUITMENT
+  }
+  // 개발
+  , {
+    guildId: process.env.DEV_GUILD_ID,
+    adminRole: process.env.DEV_GUILD_ADMIN_ROLE_ID,
+    guestRole: process.env.DEV_GUILD_GUEST_ROLE_ID,
+    guildMemberRole: process.env.DEV_GUILD_ROLE,
+    roleAuditingChannelId: process.env.DEV_ROLE_AUDITING_CHANNEL_ID,
+    channelId: process.env.DEV_CHANNEL_ID,
+    partyChannelId: process.env.DEV_PARTY_RECRUITMENT
+  }
 ];
+// guildId 별로 길드 권한이나 채널등의 아이디를 가져옴
 module.exports.getGuildInfo = (guildId) => {
   return guildInfo.find(guild => guild.guildId === guildId)
 }

64e1bf063e95635955c963ec80b7f14fd3eba9a5
minsikjeong <wjdalstlr21@gmail.com>
2024-06-28 16:12:24 +0900

    상태메세지 수정

--- events/ready/setState.js
+++ events/ready/setState.js
@@ -2,7 +2,7 @@ const { ActivityType } = require('discord.js')
 
 module.exports = (client) => {
   client.user.setActivity({
-    name: '섯다라인😎 마비노기',
+    name: '마비노기',
     type: ActivityType.Playing
   })
 }

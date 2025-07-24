deb81bb94d9d2c39c849c83bda643b5a6a55f63a
minsikjeong <wjdalstlr21@gmail.com>
2025-07-24 17:31:16 +0900

    베테랑 던전 수정

--- modules/todayMission.js
+++ modules/todayMission.js
@@ -1,8 +1,8 @@
 const axios = require('axios')
 const { DateTime } = require('luxon')
 const veteran = ['알비', '키아', '라비', '마스', '피오드', '바리', '코일', '룬다', '페카']
-
-const startDate = DateTime.local(2024, 4, 20, 0, 0)
+const startDate = DateTime.local(2024, 4, 20) // 기준일
+const today = DateTime.local();
 
 /* eslint-disable */
 Date.prototype.addDays = function (days) {
@@ -12,32 +12,13 @@ Date.prototype.addDays = function (days) {
 }
 /* eslint-disable */
 
-let veteranStartIndex = 5
-let veteranIndex = 0
-const dungeonList = [{ date: startDate, dungeon: veteran[veteranStartIndex++] }]
-for (let i = 1; i < 731; i++) {
-  if (i === 1) {
-    veteranIndex = veteranStartIndex
-  }
-  if (i > 1) {
-    veteranIndex = veteranIndex > 8 ? 0 : veteranIndex
-  }
-  const date = startDate.plus({ days: i })
-  dungeonList.push({
-    date, dungeon: veteran[veteranIndex]
-  })
-  veteranIndex++
-}
+const veteranStartIndex = 5
+const daysSinceStart = Math.floor(today.diff(startDate, 'days').days)
+const index = (veteranStartIndex + daysSinceStart) % veteran.length
+const todayVeteran = veteran[index]
+const tomorrowVeteran = veteran[index+1]
 
 module.exports = (today, now) => {
-  const todayVeteran = dungeonList.find(
-      ({ date }) => date.hasSame(today, 'day') && date.hasSame(today, 'year') &&
-          date.hasSame(today, 'month'))
-  const tomorrow = today.plus({ days: 1 })
-  const tomorrowVeteran = dungeonList.find(
-      ({ date }) => date.hasSame(tomorrow, 'day') && date.hasSame(tomorrow,
-          'year') && date.hasSame(tomorrow, 'month'))
-
   const getTodayMission = async () => {
     console.log('https://mabi.world/missions.php?server=korea&locale=korea&from=' +
         now.toISOString())
